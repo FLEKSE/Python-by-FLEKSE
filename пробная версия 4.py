977725c750cad85d35c5142033677b72bc0da270
FLEKSE <artemiy.saenko@mail.ru>
2024-03-21 10:37:05 +0300

    Update пробная версия 4.py

--- пробная версия 4.py
+++ пробная версия 4.py
@@ -1,5 +1,6 @@
-import PyQt5
-from PyQt5.QtWidgets import QApplication, QFileDialog, QMainWindow
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QPalette, QBrush, QPixmap
+from PyQt5.QtWidgets import QApplication, QFileDialog, QMainWindow, QWidget
 from PyQt5.uic import loadUi
 import sys
 import numpy
@@ -10,57 +11,132 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 from scipy.spatial import ConvexHull
 
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Программа для автоматической классификации состояния сложных технических систем")
+        Dialog.resize(830,420)
+        Dialog.setMaximumSize(830,420)
+        Dialog.setMinimumSize(830,420)
+        
+        self.filename = QtWidgets.QLineEdit(Dialog)
+        self.filename.setGeometry(QtCore.QRect(50, 60, 200, 30))
+
+        self.second_filename = QtWidgets.QLineEdit(Dialog)
+        self.second_filename.setGeometry(QtCore.QRect(480, 60, 200, 30))
+
+        self.browse = QtWidgets.QPushButton(Dialog)
+        self.browse.setGeometry(QtCore.QRect(270, 60, 80, 30))
+
+        self.browse2 = QtWidgets.QPushButton(Dialog)
+        self.browse2.setGeometry(QtCore.QRect(700, 60, 80, 30))
+
+        self.pushButton_3 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_3.setGeometry(QtCore.QRect(375, 110, 90, 30))
+
+        self.filename_2 = QtWidgets.QLineEdit(Dialog)
+        self.filename_2.setGeometry(QtCore.QRect(215, 215, 400, 25)) 
+
+        self.label_2 = QtWidgets.QLabel(Dialog)
+        self.label_2.setGeometry(QtCore.QRect(220, 213, 400, 30)) 
+
+        self.pushButton = QtWidgets.QPushButton(Dialog)
+        self.pushButton.setGeometry(QtCore.QRect(500, 300, 200, 35))
+
+        self.pushButton_2 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_2.setGeometry(QtCore.QRect(130, 300, 200, 35))
+        
+        self.filename.raise_()
+        self.second_filename.raise_()
+        self.browse.raise_()
+        self.browse2.raise_()
+        self.pushButton_3.raise_()
+        self.filename_2.raise_()
+        self.label_2.raise_()
+        self.pushButton.raise_()
+        self.pushButton_2.raise_()
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Программа для автоматической классификации состояния сложных технических систем")) #менять название
+        self.browse.setText(_translate("Dialog", "Найти"))
+        self.browse2.setText(_translate("Dialog", "Найти"))
+        self.pushButton_3.setText(_translate("Dialog", "Рассчитать"))
+        self.label_2.setText(_translate("Dialog", "Результат:"))
+        self.pushButton.setText(_translate("Dialog", "Подробнее о результате"))
+        self.pushButton_2.setText(_translate("Dialog", "Показать в пространстве"))
 
 class Program(QMainWindow):
     def __init__(self):
+        self.Error1 = True
+        self.Error2 = True
         super().__init__()
-        self.x=loadUi('C:\dont_reload\gui.ui')
-        self.x.show()
-        
 
-    def get(self):
-        list1=QFileDialog.getOpenFileName()[0]
-        df=pd.read_excel(list1, sheet_name=0, header=None)
-        matrix=df.to_numpy()
-        self.A = numpy.array(matrix)
-        self.label1=self.x.filename.setText(list1)
-     
-    def get2(self):
-        list2=QFileDialog.getOpenFileName()[0]
-        second_df=pd.read_excel(list2, sheet_name=0, header=None)
-        second_matrix=second_df.to_numpy()
-        self.B = numpy.array(second_matrix)
-        self.label2=self.x.file2.setText(list2)
+    def get(self, mode):
+        try:
+            list=QFileDialog.getOpenFileName()[0]
+            df=pd.read_excel(list, sheet_name=0, header=None)
+            matrix=df.to_numpy()
+            if mode == 0:
+                self.A = numpy.array(matrix)
+                self.label1=ui.filename.setText(list)
+                self.Error1 = False
+            elif (mode == 1):
+                self.B = numpy.array(matrix)
+                self.label2=ui.second_filename.setText(list)
+                self.Error2 = False
+            
+        except ValueError:
+            if mode == 0:
+                self.A=numpy.array([0,0,0])
+                self.label1=ui.filename.setText("Неверный формат файла")
+                self.Error1= True
+            elif (mode == 1):
+                self.B=numpy.array([0,0,0])
+                self.label2=ui.second_filename.setText("Неверный формат файла")
+                self.Error2 = True
+        except FileNotFoundError:
+            if mode == 0:
+                self.A=numpy.array([0,0,0])
+                self.Error1= True
+            elif (mode == 1):
+                self.B=numpy.array([0,0,0])
+                self.Error2 = True
 
     def schet(self):
-        U, self.D, VT=numpy.linalg.svd(self.A)
-        U2, self.D2, VT2=numpy.linalg.svd(self.B)
-        print("D", self.D)
-        print("D2", self.D2)
-        self.result = dot(self.D, self.D2)/(norm(self.D)*norm(self.D2))
-        self.result2 = norm(self.D2)-norm(self.D)
-        self.result3 = norm(self.D-self.D2)
-        print(self.result, self.result2, self.result3)
+        if self.checkErrors():
+            U, self.D, VT=numpy.linalg.svd(self.A)
+            U2, self.D2, VT2=numpy.linalg.svd(self.B)
+            print("D", self.D)
+            print("D2", self.D2)
+            self.result = dot(self.D, self.D2)/(norm(self.D)*norm(self.D2))
+            self.result2 = norm(self.D2)-norm(self.D)
+            self.result3 = norm(self.D-self.D2)
+            print(self.result, self.result2, self.result3)
+            self.defec()    
 
     def paint(self):
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
+        if self.checkErrors():
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
 
-        ax.set_xlabel('x')
-        ax.set_ylabel('y')
-        ax.set_zlabel('z')
+            ax.set_xlabel('x')
+            ax.set_ylabel('y')
+            ax.set_zlabel('z')
 
-        ax.set_xlim3d(-15,15)
-        ax.set_ylim3d(-15,15)
-        ax.set_zlim3d(-15,15)
+            ax.set_xlim3d(-15,15)
+            ax.set_ylim3d(-15,15)
+            ax.set_zlim3d(-15,15)
 
-        Point = numpy.array([self.result,self.result2,self.result3])
-        start=[0,0,0]
+            Point = numpy.array([self.result,self.result2,self.result3])
+            start=[0,0,0]
 
-        plt.plot(Point[0], Point[1], Point[2], 'or')
-        ax.quiver(start[0],start[1],start[2],self.D[0],self.D[1],self.D[2])
-        ax.quiver(start[0],start[1],start[2],self.D2[0],self.D2[1],self.D2[2],color="r")
-        plt.show()
+            plt.plot(Point[0], Point[1], Point[2], 'or')
+            ax.quiver(start[0],start[1],start[2],self.D[0],self.D[1],self.D[2])
+            ax.quiver(start[0],start[1],start[2],self.D2[0],self.D2[1],self.D2[2],color="r")
+            plt.show()
 
     def defec(self):
         defect=""
@@ -72,26 +148,44 @@ class Program(QMainWindow):
             defect="Идеально исправен"
         else:
             defect="Исправен с незначительным дефектом"
-        self.label3=self.x.filename_2.setText(f"Результат: {defect}")
+        self.label3=ui.filename_2.setText(f"                   {defect}")
         print(defect)
 
-
     def butt(self):
-        self.temp=self.x.browse.clicked.connect(self.get)
-        self.address= self.x.browse2.clicked.connect(self.get2)
-        self.but3=self.x.pushButton_3.clicked.connect(self.schet)
-        self.but3=self.x.pushButton_3.clicked.connect(self.defec)
-        self.but2=self.x.pushButton_2.clicked.connect(self.paint)
-        self.but4=self.x.pushButton.clicked.connect(self.defec)
-        # self.but5=self.x.pushButton.clicked.connect(self.second_window)
+        self.temp=ui.browse.clicked.connect(lambda: self.get(0))
+        self.address= ui.browse2.clicked.connect(lambda: self.get(1))
+        self.but3=ui.pushButton_3.clicked.connect(self.schet)
+        self.but2=ui.pushButton_2.clicked.connect(self.paint)
+        self.but5=ui.pushButton.clicked.connect(self.show_new_window)
 
+    def show_new_window(self, checked):
+        if self.checkErrors():
+            self.w = AnotherWindow()
+            self.defec()
+            self.w.show()
 
+    def checkErrors(self):
+        if not(self.Error1 or self.Error2):
+            return True
+        else:
+            self.label3=ui.filename_2.setText("                   Ошибка")
+            return False
+
+class AnotherWindow(QWidget):
+
+    def __init__(self):
+        super().__init__()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    palette = QPalette()
+    palette.setBrush(QPalette.Background, QBrush(QPixmap("C:\dont_reload\photo.jpg"))) #установить путь к фону
+    Dialog.setPalette(palette)
+    Dialog.show()
     program = Program()
-    temp = ''
-    destination = ''
-    # program.show()
     program.butt()
     sys.exit(app.exec())
